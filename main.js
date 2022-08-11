ff3a220bf6727aba0981c3183e3cdb070b50adf2
BenjieEdroso <ben09099@gmail.com>
2022-08-11 20:48:22 +0800

    cleaned

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
 import mobileNav from "./mobileNav.js";
+
 let navButton = document.querySelector(".menu-btn");
+let app = document.querySelector(".app-cont");
+let navbar = document.querySelector(".nav");
+let body = document.getElementsByTagName("body");
+
+function insertNavElement(navbarElement, bodyElement, appContElement) {
+	navbarElement.classList.add("z-10");
+	bodyElement.classList.add("overflow-hidden");
+	appContElement.insertAdjacentHTML("beforeend", mobileNav);
+}
+
+function animateInsertedNav() {
+	let navTransition = setTimeout(() => {
+		app.lastElementChild.style.transform = "translate(0%)";
+	}, 1);
+
+	return navTransition;
+}
+
+function removeInsertedNav(bodyElement, appContElement, navTransition) {
+	bodyElement.classList.remove("overflow-hidden");
+	appContElement.lastElementChild.style.transform = "translate(100%)";
+	appContElement.lastElementChild.addEventListener("transitionend", () => {
+		clearInterval(navTransition);
+		appContElement.lastElementChild.remove();
+	});
+}
+
+function scrollToPageSection(targetElement) {
+	let scrollTo = document.querySelector(`.${targetElement.target.getAttribute("data-section-type").toString()}`);
+	scrollTo.scrollIntoView();
+}
 
 navButton.addEventListener("click", () => {
-  let app = document.querySelector(".app-cont");
-  let navbar = document.querySelector(".nav");
-  navbar.classList.add("z-10");
-  let body = document.getElementsByTagName("body");
-  body[0].classList.add("overflow-hidden");
-  app.insertAdjacentHTML("beforeend", mobileNav);
-  let navTransition = setTimeout(() => {
-    app.lastElementChild.style.transform = "translate(0%)";
-  }, 1);
-  //close button
-  app.lastElementChild.children[0].addEventListener("click", () => {
-    navbar.classList.remove("z-10");
-    body[0].classList.remove("overflow-hidden");
-    app.lastElementChild.style.transform = "translate(100%)";
-    app.lastElementChild.addEventListener("transitionend", () => {
-      clearInterval(navTransition);
-      app.lastElementChild.remove();
-    });
-  });
+	insertNavElement(navbar, body[0], app);
 
-  let mobileNavLi = Array.from(app.lastElementChild.children[1].children);
-  mobileNavLi.forEach((li) => {
-    li.addEventListener("click", (e) => {
-      console.log(e.target.getAttribute("data-section-type"));
-      let scrollTo = document.querySelector(
-        `.${e.target.getAttribute("data-section-type").toString()}`
-      );
-      scrollTo.scrollIntoView();
+	let navTransition = animateInsertedNav();
+	let closeMobileNavBtn = app.lastElementChild.children[0];
+	closeMobileNavBtn.addEventListener("click", () => {
+		removeInsertedNav(body[0], app, navTransition);
+	});
 
-      body[0].classList.remove("overflow-hidden");
-      app.lastElementChild.style.transform = "translate(100%)";
-      app.lastElementChild.addEventListener("transitionend", () => {
-        clearInterval(navTransition);
-        app.lastElementChild.remove();
-      });
-    });
-  });
+	let mobileNavLinks = Array.from(app.lastElementChild.children[1].children);
+	mobileNavLinks.forEach((link) => {
+		link.addEventListener("click", (targetElement) => {
+			scrollToPageSection(targetElement);
+			removeInsertedNav(body[0], app, navTransition);
+		});
+	});
 });
 
-let desktopNav = Array.from(document.querySelector(".desktop-nav").children);
-desktopNav.forEach((link) => {
-  link.children[0].addEventListener("click", (e) => {
-    let scrollTo = document.querySelector(
-      `.${e.target.getAttribute("data-section-type").toString()}`
-    );
-    scrollTo.scrollIntoView();
-  });
+let desktopNavLinks = Array.from(document.querySelector(".desktop-nav").children);
+desktopNavLinks.forEach((link) => {
+	link.children[0].addEventListener("click", (targetElement) => {
+		scrollToPageSection(targetElement);
+		removeInsertedNav(body[0], app, navTransition);
+	});
 });
--- mobileNav.js
+++ mobileNav.js
@@ -1,7 +1,7 @@
 const mobileNav = `
 <div class="mobileNav flex items-center justify-center ">
   <button class="bg-none absolute p-7 top-0 right-0 rotate-90 transition-all duration-300 ease-in-out">
-  <img src="./assets/navbutton.png" />
+  <img src="./assets/navbutton.webp" />
   </button>
     <ul class="flex flex-col gap-10 text-center">
       <li><a href="#skills" data-section-type="skills" class="text-gray-300 text-xl">Skills</a></li>
